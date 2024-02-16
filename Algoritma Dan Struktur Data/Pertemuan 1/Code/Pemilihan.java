3c7586d2f166745e33d8a7179c201bfe9ae208b1
uhamhz <ammarhafizh2404@gmail.com>
2024-02-16 20:20:01 +0700

    feat : add Pemilihan

--- Algoritma Dan Struktur Data/Pertemuan 1/Code/Pemilihan.java
+++ Algoritma Dan Struktur Data/Pertemuan 1/Code/Pemilihan.java
@@ -1,9 +1,67 @@
+import java.util.Scanner;
+
 /**
  * Pemilihan
  */
 public class Pemilihan {
-
     public static void main(String[] args) {
-        
+        boolean nilaiValid = true;
+        String nilaiHuruf;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukan Nilai Tugas : ");
+        double tugas = input.nextDouble();
+        if (tugas > 100 || tugas < 0) {
+            nilaiValid = false;
+        }
+        System.out.print("Masukan Nilai Kuis : ");
+        double kuis = input.nextDouble();
+        if (kuis > 100 || tugas < 0) {
+            nilaiValid = false;
+        }
+
+        System.out.print("Masukan Nilai UTS : ");
+        double uts = input.nextDouble();
+        if (uts > 100 || tugas < 0) {
+            nilaiValid = false;
+        }
+
+        System.out.print("Masukan Nilai UAS : ");
+        double uas = input.nextDouble();
+        if (uas > 100 || tugas < 0) {
+            nilaiValid = false;
+        }
+
+        double nilaiAkhir = (tugas *= 0.2)+(kuis *= 0.2)+(uts *= 0.3)+(uas *= 0.3);
+
+        if (nilaiAkhir >= 80) {
+            nilaiHuruf = "A";
+        } else if (nilaiAkhir >= 73) {
+            nilaiHuruf = "B+";
+        } else if (nilaiAkhir >= 65) {
+            nilaiHuruf = "B";
+        } else if (nilaiAkhir >=60) {
+            nilaiHuruf = "C+";
+        } else if (nilaiAkhir >= 50) {
+            nilaiHuruf = "C";
+        } else if (nilaiAkhir >= 39) {
+            nilaiHuruf = "D";
+        } else {
+            nilaiHuruf = "E";
+        }
+
+        if (nilaiValid = false) {
+            System.out.println("Nilai Tidak Valid");
+        } else {
+            System.out.println("Nilai Akhir : " + nilaiAkhir);
+            System.out.println("Nilai Huruf : " + nilaiHuruf);
+            if (nilaiHuruf == "D" || nilaiHuruf == "E") {
+                System.out.println("Anda Tidak Lulus");
+            } else {
+                System.out.println("Selamat Anda Lulus");
+            }
+        }
+
+
     }
 }
