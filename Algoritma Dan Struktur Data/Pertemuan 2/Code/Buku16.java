0ea9bb2f17dca9cccb1d820f81382a31db05ea8e
uhamhz <ammarhafizh2404@gmail.com>
2024-02-21 19:42:47 +0700

    feat : add new files

---
+++ Algoritma Dan Struktur Data/Pertemuan 2/Code/Buku16.java
@@ -0,0 +1,28 @@
+public class Buku16 {
+    String judul, pengarang;
+    int halaman, stok, harga;
+
+    void tampilInformasi() {
+        System.out.println("Judul : " + judul);
+        System.out.println("Pengarang : " + pengarang);
+        System.out.println("Jumlah Halaman : " + halaman);
+        System.out.println("Sisa Stok : " + stok);
+        System.out.println("Harga : Rp " + harga);
+    }
+
+    void terjual(int jml) {
+        if (stok > 0) {
+            stok -= jml;
+        } else {
+            System.out.println("Stok Sudah Habis!");
+        }
+    }
+
+    void restock(int jml) {
+        stok += jml;
+    }
+
+    void gantiHarga(int hrg) {
+        harga = hrg;
+    }
+}
