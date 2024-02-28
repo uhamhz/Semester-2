40595f67fd1cbda89b9d65e70c3601b795d7b046
uhamhz <ammarhafizh2404@gmail.com>
2024-02-28 22:21:46 +0700

    feat : renew

--- Algoritma Dan Struktur Data/Pertemuan 3/Code/ArraysObjects/ArrayObjects.java
+++ Algoritma Dan Struktur Data/Pertemuan 3/Code/ArraysObjects/ArrayObjects.java
@@ -1,27 +1,30 @@
 package Code.ArraysObjects;
 
+import java.util.Scanner;
+
 /**
  * ArrayObjects
  */
 public class ArrayObjects {
-
     public static void main(String[] args) {
-        PersegiPanjang[] ppArray = new PersegiPanjang[3];
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Masukan Jumlah Persegi Panjang : ");
+        int jumlah = sc.nextInt();
+        PersegiPanjang[] ppArray = new PersegiPanjang[jumlah];
+        
 
-        ppArray[0] = new PersegiPanjang();
-        ppArray[0].panjang = 110;
-        ppArray[0].lebar = 30;
-
-        ppArray[1] = new PersegiPanjang();
-        ppArray[1].panjang = 80;
-        ppArray[1].lebar = 40;
-
-        ppArray[2] = new PersegiPanjang();
-        ppArray[2].panjang = 100;
-        ppArray[2].lebar = 20;
-
-        System.out.println("Persegi Panjang ke-0, panjang : " + ppArray[0].panjang + ", lebar : " + ppArray[0].lebar);
-        System.out.println("Persegi Panjang ke-1, panjang : " + ppArray[1].panjang + ", lebar : " + ppArray[1].lebar);
-        System.out.println("Persegi Panjang ke-2, panjang : " + ppArray[2].panjang + ", lebar : " + ppArray[2].lebar);
+        for (int i = 0; i < ppArray.length; i++) {
+            ppArray[i] = new PersegiPanjang();
+            System.out.println("Persegi Panjang Ke-" + i);
+            System.out.print("Masukan Panjang : ");
+            ppArray[i].panjang = sc.nextInt();
+            System.out.print("Masukan Lebar : ");
+            ppArray[i].lebar = sc.nextInt();
+        }
+        for (int i = 0; i < ppArray.length; i++) {
+            System.out.println("Persegi Panjang ke-" + i);
+            System.out.println("Panjang : " + ppArray[i].panjang + ", Lebar : " + ppArray[i].lebar);
+        }
     }
 }
