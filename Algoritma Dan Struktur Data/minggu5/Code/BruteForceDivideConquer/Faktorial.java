213a1260cd368ba08f1b56bd6c554b5d664b988d
uhamhz <ammarhafizh2404@gmail.com>
2024-03-16 05:03:47 +0700

    feat : update

--- Algoritma Dan Struktur Data/minggu5/Code/BruteForceDivideConquer/Faktorial.java
+++ Algoritma Dan Struktur Data/minggu5/Code/BruteForceDivideConquer/Faktorial.java
@@ -6,8 +6,10 @@ public class Faktorial {
 
     int faktorialBF(int n) {
         int fakto = 1;
-        for (int i = 1; i <= n; i++) {
+        int i = 1;
+        while (i <= n) {
             fakto = fakto * i;
+            i++;
         }
         return fakto;
     }
